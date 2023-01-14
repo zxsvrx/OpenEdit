4ce8c24f195fb78c51ff59aca577ff3133eadde4
zxsvrx <122689944+zxsvrx@users.noreply.github.com>
2023-01-14 18:13:29 +0100

    Update TextEditor.hpp

--- src/TextEditor.hpp
+++ src/TextEditor.hpp
@@ -1,6 +1,24 @@
 #ifndef TextEditor_hpp
 #define TextEditor_hpp
 
+#include "Text.hpp"
 
+class TextEditor : public Text {
+  unsigned int cursorX;
+  unsigned int cursorY;
+  unsigned int selectionStart;
+  unsigned int selectionEnd;
+  std::vector<PreReader> activePreReaders;
+  public:
+  
+  int addPreReader(PreReader & pr);
+  void removePreReader(int id);
+  
+  void setCursor(unsigned int x, unsigned int y);
+  void moveCursor(int x, int y);
+  
+  void add(char c); 
+  void remove();
+}
 
 #endif /* TextEditor_hpp */
