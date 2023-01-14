bec3c0d2d3c928c44a65020524212cee5145af06
zxsvrx <122689944+zxsvrx@users.noreply.github.com>
2023-01-14 18:15:35 +0100

    Update DisplayText.hpp

--- src/DisplayText.hpp
+++ src/DisplayText.hpp
@@ -1,4 +1,12 @@
 #ifndef DisplayText_hpp
 #define DisplayText_hpp
 
+class DisplayText {
+  StyleSheet styleSheet;
+  public:
+  
+  void setStyleSheet(StyleSheet & _styleSheet);
+  std::string getText(std::string & text);
+};
+
 #endif /* DisplayText_hpp */
